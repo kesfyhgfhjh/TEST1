6d68607b0cad59b89ad9f5a2ba4058339e3ea007
kesfyhgfhjh <fukefoll1@gmail.com>
2025-08-25 15:36:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os, logging
+
+logging.basicConfig(format='𝑻𝒈 » @𝒎𝒔𝒘𝒑𝒓𝒆𝒔𝒆𝒏𝒕 - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+API_ID = int(os.environ.get("API_ID", "26416404"))
+API_HASH = os.environ.get("API_HASH", "655b8dc1517dacb007deb53fc0fda30d")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8021024742:AAF9Vl1YHRnUxTXOQ6pMnOwxDrEG_BPu0Vg")
+
+#Coded by Mr. Ankush Tg: @Coder_ankushBot
+
+#Password: @Coder_ankushBot
